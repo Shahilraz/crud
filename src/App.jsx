1396436fce3f6ae061d09419d4a47b875e36c18d
Movie_Website <sahilkumaraz@gmail.com>
2024-12-25 22:22:23 +0530

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import {getPost} from './api/Postapi'
+
 
 function App() {
+  console.log(getPost())
+  const getPostData=async()=>{
+    const result = await getPost();
+    console.log(res);
+  };
+
+  useEffect(()=>{
+    getPostData();
+  },[]);
+
+
+
+
   return (
+    
     <div>
+    
       hello react
     </div>
   )
